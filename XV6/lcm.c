7cea2a7cefe8207a255745d525b3f42733ccb0db
hamidkhd <hrkh78@gmail.com>
2020-10-02 03:02:26 +0330

    lcm without write in file.

---
+++ XV6/lcm.c
@@ -0,0 +1,36 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int find_gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    else
+        return find_gcd(b % a, a);     
+} 
+
+int find_lcm(int array[], int n) 
+{ 
+    int result = array[0]; 
+  
+    for (int i = 1; i < n; i++) 
+    {
+        result = ((result * array[i]) / find_gcd(result, array[i])); 
+    }
+  
+    return result;
+} 
+
+int main(int argc, char *argv[])
+{  
+    int *arr;
+    arr = (int*)malloc((argc-1)* sizeof(int));
+
+    for (int i = 0; i < (argc-1); i++) 
+        arr[i] = atoi(argv[i+1]);
+
+    printf(1, "%d\n", find_lcm(arr, argc-1));
+    
+    exit();  
+}  
