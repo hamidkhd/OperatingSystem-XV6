1436cfbda09d0c61c636f5b97aa5162ebdf56c52
hamidkhd <hrkh78@gmail.com>
2020-10-07 12:44:03 +0330

    Repair lcm.c file.

--- XV6/lcm.c
+++ XV6/lcm.c
@@ -3,67 +3,52 @@
 #include "user.h"
 #include "fcntl.h"
 
-int count_digit(int number)
+int count_digit(int number, int digit)
 {
-    int digit = 0;
-
-    while (number)
-    {
-        digit++;
-        number /= 10;
-    }
+    if (number)
+        return count_digit(number/10, ++digit);
     return digit;
 }
 
-char *int_to_char_array(int number)
+char *int_to_char_array(int number, int digit)
 {
-    int digit = count_digit(number);
     char* array = (char*)malloc(digit);
-    char* temp_array = (char*)malloc(digit);
     
-    int counter = 0;
-
     if (number == 0)
     {
-        array[counter] = '0'; 
-        array[++counter] = '\0';       
-        //array[++counter] = '\n';
-        return array;
+        array[0] = '0'; 
+        return (char*) array;
     }
     else 
     {
-        while (number != 0) 
+        for (int i = 0; i < digit; i++) 
         {
-            counter++;
-            temp_array[counter] = number % 10 + 48;
+            array[digit-i-1] = number % 10 + 48;
             number /= 10;
         }
-
-        for (int i = 0; i < counter; i++) 
-            array[i] = temp_array[counter- i];
-        
-        array[counter] = '\n';
-        array[++counter] = '\0';
     }
-    return array; 
+
+    return (char*) array; 
 }
 
 int find_gcd(int a, int b)
 {
-    if (a == 0)
-        return b;
-    else
-        return find_gcd(b % a, a);     
+    int gcd = 1, counter = 1;
+    while (counter <= a && counter <= b)
+    {
+        if (a % counter == 0 && b % counter == 0) 
+            gcd = counter;
+        counter++;
+    } 
+    return gcd; 
 } 
 
 int find_lcm(int array[], int n) 
 { 
     int result = array[0]; 
-  
+
     for (int i = 1; i < n; i++) 
-    {
-        result = ((result * array[i]) / find_gcd(result, array[i])); 
-    }
+        result *= (array[i] / find_gcd(result, array[i])); 
   
     return result;
 } 
@@ -76,12 +61,15 @@ int main(int argc, char *argv[])
     for (int i = 0; i < (argc-1); i++) 
         arr[i] = atoi(argv[i+1]);
 
-    char* result = int_to_char_array(find_lcm(arr, argc-1));
-    printf(1,"%d\n",strlen(result));
+    int number = find_lcm(arr, argc-1);
+    int digit = count_digit(number, 0);
+    
+    char* result = int_to_char_array(number, digit);
 
     int file = open("lcm_result.txt", O_CREATE | O_WRONLY);
-    write(file, result, 5);
+    write(file, result, digit);
+    write(file, "\0\n", 2);
     close(file);
-
+    
     exit();  
 }  
