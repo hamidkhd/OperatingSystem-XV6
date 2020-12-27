ed1a833d5c7687c9fc0b425305165d621e9d1776
hamidkhd <hrkh78@gmail.com>
2020-12-27 14:57:41 +0330

    add user spinlock

---
+++ p4/user_spinlock.c
@@ -0,0 +1,28 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "x86.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "spinlock.h"
+
+struct sleeplock {
+  uint locked;     
+  struct spinlock lk;     
+};
+
+void initlock(struct spinlock *lk, char *name)
+{
+  lk->locked = 0;
+}
+
+void acquire(struct spinlock *lk)
+{
+  while(xchg(&lk->locked, 1) != 0);
+}
+
+void release(struct spinlock *lk)
+{
+  asm volatile("movl $0, %0" : "+m" (lk->locked) : );
+}
